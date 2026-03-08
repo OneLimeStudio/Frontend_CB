d3b8882b349c47f646d6bb170c58e3920a99a605
Vanshdeep Singh <62259024+OneLimeStudio@users.noreply.github.com>
2026-03-08 17:30:49 +0530

    Update base path for production mode in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/your-repo-name/" : "/",
+  base: mode === "production" ? "/Frontend_CB/" : "/",
   server: {
     host: "::",
     port: 8080,
